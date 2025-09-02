9c2d4908c2bcf9e7aca21be81b7928c0e6dad7d0
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-09-02 14:32:24 -0600

    feat: render products dynamically from JSON file

---
+++ main.js
@@ -0,0 +1,25 @@
+const $dessertsContariner = document.querySelector(".desserts-container");
+
+loadProducts();
+
+function loadProducts() {
+    try {
+        fetch('./data.json')
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach(product => {
+                    const $product = document.createElement("article");
+                    $product.innerHTML = `<img src="${product.image.mobile}" alt="${product.name}">
+                                        <h3 class="category-product">${product.category}</h3>
+                                        <h4 class="name-product">${product.name}</h4>
+                                        <p class="price-product">${product.price}</p>
+                                        <div class="actions-product">
+                                        <button>Add to Cart</button>
+                                        </div>`
+                    $dessertsContariner.appendChild($product);
+                });
+            });
+    } catch (error) {
+        console.log.error("Error: " + error);
+    }
+}
