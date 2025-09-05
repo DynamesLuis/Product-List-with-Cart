1c83daf99c114cbf88bee9529abac762d65ec7f9
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-09-05 17:25:24 -0600

    refactor: load product with async/await and main function for await loadProducts

--- main.js
+++ main.js
@@ -3,36 +3,45 @@ const $confirmBtn = document.querySelector(".confirm-btn");
 const $newOrdenBtn = document.querySelector(".newOrden-btn");
 const $overlay = document.querySelector(".overlay");
 
-initEvents();
-loadProducts();
+main();
 
+
+async function main() {
+    await loadProducts();
+    initEvents();
+}
 
 function initEvents() {
     $confirmBtn.addEventListener('click', showPopup);
     $newOrdenBtn.addEventListener('click', hidePopup);
+    
+
 }
-function loadProducts() {
+
+
+
+async function loadProducts() {
     try {
-        fetch('./data.json')
-            .then((response) => response.json())
-            .then((data) => {
-                data.forEach(product => {
-                    const $product = document.createElement("article");
-                    $product.innerHTML = `<img src="${product.image.mobile}" alt="${product.name}">
-                                        <h3 class="category-product">${product.category}</h3>
-                                        <h4 class="name-product">${product.name}</h4>
-                                        <p class="price-product">$${product.price}</p>
-                                        <div class="actions-product">
-                                            <button class="add-btn"> <img src="./assets/images/icon-add-to-cart.svg" alt="ad-to-cart"> Add to Cart</button>
-                                            <div class="count-product">
-                                                <button class="decrement-btn"></button>
-                                                <p>1</p>
-                                                <button class="increment-btn"></button>
-                                            </div>
-                                        </div>`
-                    $dessertsContariner.appendChild($product);
-                });
-            });
+        const response = await fetch("./data.json");
+        const result = await response.json();
+        result.forEach(product => {
+            const $product = document.createElement("article");
+            $product.classList.add("product");
+            $product.innerHTML = `<img src="${product.image.mobile}" alt="${product.name}">
+                                  <h3 class="category-product">${product.category}</h3>
+                                  <h4 class="name-product">${product.name}</h4>
+                                  <p class="price-product">$${product.price}</p>
+                                  <div class="actions-product">
+                                     <button class="add-btn"> <img src="./assets/images/icon-add-to-cart.svg" alt="ad-to-cart"> Add to Cart</button>
+                                     <div class="count-product">
+                                        <button class="decrement-btn"></button>
+                                        <p>1</p>
+                                        <button class="increment-btn"></button>
+                                     </div>
+                                  </div>`
+            $dessertsContariner.appendChild($product);
+        });
+
     } catch (error) {
         console.log.error("Error: " + error);
     }
